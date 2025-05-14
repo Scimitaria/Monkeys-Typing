25bcacbd1458a8af2b54cf04a48b183102aa8b9a
Morgan Powers <morganpowers@Jessicas-MacBook-Air-5.local>
2025-05-14 08:57:23 -0500

    unused imports corrected

--- app/src/main/java/cs2/AIP/Mario.java
+++ app/src/main/java/cs2/AIP/Mario.java
@@ -1,18 +1,17 @@
 package cs2.AIP;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import java.util.ArrayList;
-import java.util.List;
-
 import cs2.util.Vec2;
+import java.util.List;
+import java.util.ArrayList;
+import javafx.application.Application;
 import javafx.animation.AnimationTimer;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.StackPane;
+import javafx.scene.canvas.GraphicsContext;
 
 //run with ./gradlew run -Pmain=cs2.AIP.Mario --args="<args>"
 public class Mario extends Application{
@@ -23,6 +22,7 @@ public class Mario extends Application{
     @Override
     public void start(Stage stage) {
         Image placeholder   = new Image("file:square.png");
+        Image flagimg       = new Image("flag.png");
         stage.setTitle("Monkeys Typing");
         Canvas canvas = new Canvas(800, 800);
         stage.setScene(new Scene(new StackPane(canvas)));
@@ -44,11 +44,43 @@ public class Mario extends Application{
             int frame = 0;
             int reps  = 1;
             Eugenics e  = new Eugenics(placeholder,population,initActions);
+            AI goal     = new AI(flagimg,new Vec2(650, 400),new ArrayList<Integer>());
+
+            public double distToGoal(AI ai){
+                var pos=ai.pos;
+                var gpos=goal.pos;
+                var a=Math.pow(Math.abs(pos.getX()-gpos.getX()),2);
+                var b=Math.pow(Math.abs(pos.getY()-gpos.getY()),2);
+                return Math.sqrt(a+b);
+            }
+            double initDist = distToGoal(e.kids.get(0));
 
             @Override
             public void handle(long now) {
-                //TODO: make Mario
+                g.fillRect(0, 0, 800, 800);
+
+                for (int i = 0; i < e.kids.size() - 1; i++) {
+                    var kid=e.kids.get(i);
+
+                    //TODO: make Mario
+
+                    kid.display(g);
+
+                    if(kid.intersect(goal))System.exit(0);
+
+                    //success evaluation
+                    Double diff = initDist-distToGoal(kid);
+                    kid.pro = diff.intValue();
+                }
+
                 frame++;
+                var lenActs=e.kids.get(0).actions.size()-1;
+                if(frame>=lenActs){
+                    //can switch implementations of evolve to switch algorithms
+                    e.evolve(e.kids,placeholder,addNum,(reps%addRate)==0);
+                    frame=0;
+                    reps++;
+                }
             }
         };
         timer.start();
--- app/src/main/java/cs2/AIP/Playground.java
+++ app/src/main/java/cs2/AIP/Playground.java
@@ -1,17 +1,17 @@
 package cs2.AIP;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import java.util.ArrayList;
-import java.util.List;
 import cs2.util.Vec2;
+import java.util.List;
+import java.util.ArrayList;
+import javafx.application.Application;
 import javafx.animation.AnimationTimer;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.StackPane;
+import javafx.scene.canvas.GraphicsContext;
 
 //run with ./gradlew run -Pmain=cs2.AIP.Playground --args="<args>"
 public class Playground extends Application {
