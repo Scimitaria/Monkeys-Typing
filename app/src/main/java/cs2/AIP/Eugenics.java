ce8e2fdb37c16f8f7947b33ebf812c566db0fdbd
Morgan Powers <morgan.c.powers@gmail.com>
2025-05-14 14:12:13 -0500

    setting up branches

--- app/src/main/java/cs2/AIP/Eugenics.java
+++ app/src/main/java/cs2/AIP/Eugenics.java
@@ -1,3 +1,4 @@
+
 package cs2.AIP;
 
 import cs2.util.Vec2;
@@ -8,8 +9,9 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class Eugenics {
-  public ArrayList<AI> kids = new ArrayList<AI>();      //agents
-  ArrayList<Integer>   acts = new ArrayList<Integer>(); //actions
+  public ArrayList<AI> kids = new ArrayList<AI>();    //agents
+  ArrayList<Integer> acts = new ArrayList<Integer>(); //actions
+  ArrayList<Integer> pros = new ArrayList<Integer>(); //success evaluation
   Random rand = new Random();
 
   public Eugenics(Image ick, int population, int actionNum) {
@@ -20,27 +22,22 @@ public class Eugenics {
     }
   }
 
-  //custom instance of sort for AI
-  public void sort(ArrayList<AI> AIlst){
-    Collections.sort(kids,new Comparator<AI>(){
-      public int compare(AI a1, AI a2){
-        if(a1.pro>a2.pro) return 1;
-        if(a1.pro<a2.pro) return -1;
-        return 0;
-      }
-    });
-  }
-
   //mixes attributes of the best agents to create the next generation
   //TODO: check w/ more than two
-  public ArrayList<Integer> procreate(ArrayList<AI> ai){
+  public ArrayList<Integer> procreate(ArrayList<Integer> a1,ArrayList<Integer> a2){
+    var len = a1.size();
+    if(len!=a2.size())throw new IndexOutOfBoundsException("lists are not of equal length");
+
     var kid=new ArrayList<Integer>();
     var coinflip=0;
+    var e = 0;
 
     //pick random between selections
-    for(int i=0;i<ai.get(0).actions.size();i++){
-      coinflip=rand.nextInt(ai.size()-1);
-      kid.add(i,ai.get(coinflip).actions.get(i));
+    for(int i=0;i<len;i++){
+      coinflip=rand.nextInt(2);
+      if(coinflip==1)e=a1.get(i);
+      else e=a2.get(i);
+      kid.add(i,e);
     }
     return kid;
   }
@@ -50,46 +47,46 @@ public class Eugenics {
    * based on success criteria. The best ones will
    * be selected to produce the next generation.
   */
-  public void evolve(ArrayList<AI> ai,Image image,int numParents,int addNum,boolean addRate){
+  public void evolve(ArrayList<AI> ai,Image image,int addNum,boolean addRate){
+    var len = ai.size();
     var children = new ArrayList<AI>();
 
     //sort AI based on success evaluation
-    sort(ai);
+    Collections.sort(ai,new Comparator<AI>(){
+      public int compare(AI a1, AI a2){
+        if(a1.pro>a2.pro) return 1;
+        if(a1.pro<a2.pro) return -1;
+        return 0; 
+      }
+    });
     //select top performers
-    ArrayList<AI> parents = new ArrayList<>(ai.subList(0, Math.min(numParents, ai.size())));
+    var first  = ai.get(len-1);
+    var second = ai.get(len-2);
 
+    //reset actions
+    acts=new ArrayList<Integer>();
     for(int i=0;i<kids.size();i++){
-      //reset actions
-      acts=new ArrayList<Integer>();
-      acts=procreate(parents);
-      //System.out.println(acts.size());
+      acts=procreate(first.actions,second.actions);
       if(addRate) for(int j=0;j<addNum;j++) acts.add(rand.nextInt(4));
-      //System.out.println(acts.size());
       children.add(new AI(image,new Vec2(400, 400),acts));
     }
     kids=children;
-    //System.out.println(kids.get(0).actions.size());
-    //System.out.println(acts.size());
   }
 
-
-  //randomly generate new actions
   public AI type(AI ai,int level, int div){
     var len = ai.actions.size();
 
     //randomize based on success evaluation
     for(int i=0;i<len;i++){
-      //vary randomly based on level
-      //level is based on success eval
       switch (level){
         case 2: for(int j = 0; j < len - 1; j += rand.nextInt(div / 10)) ai.actions.set(i, rand.nextInt(4)); break;
         case 1: for(int j = 0; j < len - 1; j += rand.nextInt(div / 2)) ai.actions.set(i, rand.nextInt(4)); break;
       }
     }
-
     return ai;
   }
 
+  //TODO: fix MT
   /*Monkeys Typing variant
    * Similar concept to the Genetic Algorithm,
    * but without the selection of top agents.
@@ -100,23 +97,19 @@ public class Eugenics {
    * This algorithm is terrible lol
   */
   public void evolve(int addNum,boolean addRate) {
+    pros = new ArrayList<Integer>();
     var len=kids.size();
-
-    //sort AI based on success evaluation
-    sort(kids);
-
-    for (int fg = 0; fg < kids.size(); fg++) {
+    for (int j = 0; j < len; j++) pros.add(kids.get(j).pro);
+    Collections.sort(pros);
+    for (int fg = 0; fg < pros.size(); fg++) {
+      var p=pros.get(fg);
       var kid=kids.get(fg);
-      //reset position
-      kid.pos = new Vec2(400, 400);
-
-      //least successful change most
-      if(kid.pro < len / 2) type(kid,2,len);
-      //more successful change less; most successful don't change
-      if((fg > (len / 2)) && (fg < len * (3 / 4))) type(kid,1,len);
-
-      //add additional random actions
-      if(addRate) for(int j=0;j<addNum;j++) kid.actions.add(rand.nextInt(4));
+      if (p == kid.pro){
+        kid.pos = new Vec2(400, 400);
+        if(p < len / 2) type(kid,2,len);
+        if((fg > 9) && (fg < len * 3 / 4)) type(kid,2,len);
+        if(addRate) for(int j=0;j<addNum;j++) acts.add(rand.nextInt(4));
+      }
     }
   }
 }
--- app/src/main/java/cs2/AIP/Playground.java
+++ app/src/main/java/cs2/AIP/Playground.java
@@ -1,61 +1,40 @@
 package cs2.AIP;
 
-import cs2.util.Vec2;
-import java.util.List;
-import java.util.ArrayList;
 import javafx.application.Application;
-import javafx.animation.AnimationTimer;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.image.Image;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.layout.StackPane;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+import cs2.util.Vec2;
+import javafx.animation.AnimationTimer;
 
-//run with ./gradlew run -Pmain=cs2.AIP.Playground --args="<args>"
 public class Playground extends Application {
-    public static void main(String[] args) {
-        // Forward the raw command‑line arguments to JavaFX
-        Application.launch(Playground.class, args);
-    }
-
-    public Integer toInt(String s){
-        return Integer.parseInt(s);
-    }
-
     @Override
     public void start(Stage stage) {
         Image image   = new Image("file:square.png");
         Image goalimg = new Image("file:goal.jpg");
         Image obsimg  = new Image("file:obs.png");
-        Canvas canvas = new Canvas(800, 800);
         stage.setTitle("Monkeys Typing");
+        Canvas canvas = new Canvas(800, 800);
         stage.setScene(new Scene(new StackPane(canvas)));
         GraphicsContext g = canvas.getGraphicsContext2D();
         g.setFill(Color.WHITE);
         stage.show();
-
-        // Retrieve them here
-        Parameters params = getParameters();
-        // The raw, position‑based args exactly as typed
-        List<String> args = params.getRaw();
-
-        //TODO: get arg assignment to work
-        int population  = 50; //size of starting population
+        //TODO: make these arg accessible
+        int population  = 50;//size of starting population
         int initActions = 100;//# of actions to start
-        int addRate = 1;  //# of turns between adding actions
+
+        int addRate = 1; //# of turns between adding actions
         int addNum  = 100;//# of actions to add
-        int numParents = 2; //# of parents for new generation
-        boolean algorithmToggle = true;
 
         AnimationTimer timer = new AnimationTimer() {
             int frame = 0;
             int reps  = 1;
-
             Eugenics e  = new Eugenics(image,population,initActions);
-            ArrayList<AI> kids = e.kids;
-
             AI goal     = new AI(goalimg,new Vec2(650, 400),new ArrayList<Integer>());
             AI obstacle = new AI(obsimg,new Vec2(500, 375),new ArrayList<Integer>());
 
@@ -66,13 +45,13 @@ public class Playground extends Application {
                 var b=Math.pow(Math.abs(pos.getY()-gpos.getY()),2);
                 return Math.sqrt(a+b);
             }
-            double initDist = distToGoal(kids.get(0));
+            double initDist = distToGoal(e.kids.get(0));
 
             @Override
             public void handle(long now) {
                 g.fillRect(0, 0, 800, 800);
-                for (int i = 0; i < kids.size() - 1; i++) {
-                    var kid = kids.get(i);
+                for (int i = 0; i < e.kids.size() - 1; i++) {
+                    var kid=e.kids.get(i);
                     //take action
                     //TODO: make obstacle work
                         switch(kid.actions.get(frame)){
@@ -92,16 +71,12 @@ public class Playground extends Application {
                 }
                 goal.display(g);
                 obstacle.display(g);
-
+                
                 frame++;
-                var lenActs = kids.get(0).actions.size()-1;
-                if(frame >= lenActs){
+                var lenActs=e.kids.get(0).actions.size()-1;
+                if(frame>=lenActs){
                     //can switch implementations of evolve to switch algorithms
-                    //TODO: parameterize algorithm switch
-                    if(algorithmToggle)
-                         e.evolve(kids,image,numParents,addNum,(reps%addRate)==0);
-                    else e.evolve(addNum,(reps%addRate)==0);
-
+                    e.evolve(e.kids,image,addNum,(reps%addRate)==0);
                     frame=0;
                     reps++;
                 }
