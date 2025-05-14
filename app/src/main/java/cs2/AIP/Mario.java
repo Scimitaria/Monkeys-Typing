aafa6cb684f0e435580ac8d1f3bb33dd9e841551
Morgan Powers <morgan.c.powers@gmail.com>
2025-05-14 16:20:18 -0500

    documentation

--- app/src/main/java/cs2/AIP/Mario.java
+++ app/src/main/java/cs2/AIP/Mario.java
@@ -39,7 +39,8 @@ public class Mario extends Application{
         int initActions = 100;//# of actions to start
         int addRate = 1;  //# of turns between adding actions
         int addNum  = 100;//# of actions to add
-        int numParents = 2; //# of parents for new generation
+        int numParents = 5; //# of parents for new generation
+        boolean algorithmToggle = true;
 
         AnimationTimer timer = new AnimationTimer() {
             int frame = 0;
@@ -81,7 +82,9 @@ public class Mario extends Application{
                 var lenActs = kids.get(0).actions.size()-1;
                 if(frame >= lenActs){
                     //can switch implementations of evolve to switch algorithms
-                    //e.evolve(kids,placeholder,numParents,addNum,(reps%addRate)==0);
+                    if(algorithmToggle)
+                         e.evolve(e.kids,placeholder,numParents,addNum,(reps%addRate)==0);
+                    else e.evolve(addNum,(reps%addRate)==0);
                     frame=0;
                     reps++;
                 }
